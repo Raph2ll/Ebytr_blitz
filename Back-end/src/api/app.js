7d6dcc7f2846f811a82052844e26a4d7fda03e72
Raph2ll <raphael.ph2ll@gmail.com>
2022-02-15 19:24:40 -0300

    backend - configurando eslint com o da trybe

--- Back-end/src/api/app.js
+++ Back-end/src/api/app.js
@@ -1,1 +1,20 @@
-//.
+const express = require('express');
+const cors = require('cors');
+const route = require('./modules/routes.js');
+const error = require('./modules/global/middlewares/error');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => res
+.status(200)
+.send('<h1>Bem-vindo ao projeto Ebytr</h1>'));
+
+app.use('/', route);
+
+app.use(error);
+
+module.exports = app;
